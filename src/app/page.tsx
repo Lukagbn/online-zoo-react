52d1b78946bfe2642fa5a811ad1fcda5aeafd287
lukagbn <luka68690@gmail.com>
2026-03-25 15:52:05 +0400

    feat: create new donations component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,66 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import layout from "./layout.module.scss";
 import Link from "next/link";
+import RightArrow from "@/components/RightArrow/RightArrow";
+import DonationBanner from "@/components/DonationBanner/DonationBanner";
 
 export default function Home() {
   return (
-    <section className={styles.hero}>
-      <div className={`${styles.heroContainer} ${layout.container}`}>
-        <div className={styles.heroContent}>
-          <h2>Watch your favorite animal online</h2>
-          <p>
-            Explore the exciting and mysterious world of wild animals in a
-            natural setting without leaving your home.
-          </p>
-          <Link className={styles.viewLive} href="#">
-            VIEW LIVE CAM
-            <svg
-              width="25"
-              height="22"
-              viewBox="0 0 25 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M13.2098 0.119971C13.0277 0.199174 12.8622 0.315255 12.7229 0.461565C12.5833 0.607505 12.4725 0.780876 12.397 0.971748C12.3214 1.16262 12.2825 1.36724 12.2825 1.57389C12.2825 1.78055 12.3214 1.98517 12.397 2.17604C12.4725 2.36691 12.5833 2.54028 12.7229 2.68622L18.7506 9H1.6C1.17565 9 0.768688 9.21071 0.468629 9.58579C0.168571 9.96086 0 10.4696 0 11C0 11.5304 0.168571 12.0391 0.468629 12.4142C0.768688 12.7893 1.17565 13 1.6 13H18.7514L12.7229 19.3146C12.4414 19.6096 12.2833 20.0097 12.2833 20.4269C12.2833 20.8441 12.4414 21.2443 12.7229 21.5393C13.0045 21.8343 13.3863 22 13.7845 22C14.1826 22 14.5645 21.8343 14.846 21.5393L23.842 12.1127C23.9816 11.9668 24.0924 11.7934 24.168 11.6026C24.2436 11.4117 24.2825 11.2071 24.2825 11.0004C24.2825 10.7938 24.2436 10.5891 24.168 10.3983C24.0924 10.2074 23.9816 10.034 23.842 9.88808L14.846 0.461565C14.7067 0.315255 14.5413 0.199174 14.3591 0.119971C14.177 0.0407677 13.9817 0 13.7845 0C13.5873 0 13.392 0.0407677 13.2098 0.119971Z"
-                fill="white"
-              />
-            </svg>
-          </Link>
+    <>
+      <section className={styles.hero}>
+        <div className={`${styles.heroContainer} ${layout.container}`}>
+          <div className={styles.heroContent}>
+            <h2>Watch your favorite animal online</h2>
+            <p>
+              Explore the exciting and mysterious world of wild animals in a
+              natural setting without leaving your home.
+            </p>
+            <Link className={styles.viewLive} href="#">
+              VIEW LIVE CAM
+              <RightArrow color="white" />
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className={styles.heroIntro}>
+        <div className={layout.container}>
+          <div className={styles.introWelcome}>
+            <img src="/images/online-zoo.png" alt="online zoo" />
+            <div className={styles.heroIntroTextContent}>
+              <h2>Welcome to the Online Zoo!</h2>
+              <p>
+                On our website, using live webcams, fans of all ages can observe
+                various animals. Among them, are Giant pandas, eagles,
+                alligators, forest gorillas, African lions, and others. It is
+                the whole natural world in real-time in front of our cameras. We
+                hope you will enjoy watching closely and explore animals’
+                behavior and habitats! Note: animals are not always on view on
+                cameras, so please check back if you don't see anything.
+              </p>
+            </div>
+          </div>
+          <div className={styles.introHow}>
+            <img src="/images/eagles.png" alt="eagles" />
+            <div className={styles.heroIntroTextContent}>
+              <h2>How we work</h2>
+              <p>
+                Online Zoo is a nonprofit committed to inspiring awareness and
+                preservation of nature and wild animals in our zoo and
+                worldwide. Every day, our experts work to safeguard the health
+                and wellness of the animals. To continue these efforts, we need
+                your help. We're so grateful to our numerous supporters. All
+                donations, large and small, go a long way to the conservation
+                efforts of our pets.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <DonationBanner
+        title="Your donation makes a difference!"
+        paragraph="The Online Zoo's animal webcams are some of the most famous on the
+              internet. Tune in to watch your favourite animals — live, 24/7!"
+      />
+    </>
   );
 }
---
+++ src/components/DonationBanner/DonationBanner.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./DonationBanner.module.scss";
+import layout from "@/app/layout.module.scss";
+import RightArrow from "../RightArrow/RightArrow";
+
+interface DonationProps {
+  title: string;
+  paragraph: string;
+}
+
+function DonationBanner({ title, paragraph }: DonationProps) {
+  return (
+    <section className={styles.donations}>
+      <div className={`${styles.donationContainer} ${layout.container}`}>
+        <div className={styles.donationsTextContent}>
+          <div className={styles.donationInfo}>
+            <h2>{title}</h2>
+            <p>{paragraph}</p>
+          </div>
+          <div className={styles.donationAction}>
+            <h3>Quick Donate</h3>
+            <button type="button">
+              $ donation amount
+              <div className={styles.arrowBtnBackground}>
+                <RightArrow color="white" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DonationBanner;
