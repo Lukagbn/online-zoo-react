11a2d27e250fc6bae79e1cae04e2ef95ed55f7c5
lukagbn <luka68690@gmail.com>
2026-03-31 18:48:56 +0400

    style: add background on active animal box in aside component

--- src/components/Aside/AsideBox/AsideBox.tsx
+++ src/components/Aside/AsideBox/AsideBox.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from "next/link";
 import styles from "./AsideBox.module.scss";
 import { ASIDE_ICONS } from "@/utils/animalIcons";
+import { usePathname } from "next/navigation";
 
 function AsideBox({
   id,
@@ -11,11 +13,27 @@ function AsideBox({
   title: string;
   expand: boolean;
 }) {
+  const pathname = usePathname();
   const icon = ASIDE_ICONS.find((icon) => icon.id.toString() == id);
+  const isActive = pathname == `/zoos/${id}`;
   return (
-    <div className={styles.asideBox}>
+    <div
+      className={
+        isActive
+          ? `${styles.asideBox} ${styles.asideBoxActive}`
+          : `${styles.asideBox}`
+      }
+    >
       <Link href={`/zoos/${id}`}>
-        <img className={styles.panda} src={`${icon?.icon}`} alt="animal" />
+        <div
+          className={
+            isActive
+              ? `${styles.imgWrapper} ${styles.imgWrapperActive}`
+              : `${styles.imgWrapper}`
+          }
+        >
+          <img className={styles.animal} src={`${icon?.icon}`} alt="animal" />
+        </div>
       </Link>
       <p className={expand ? `${styles.expand}` : `${styles.collapse}`}>
         {title}
